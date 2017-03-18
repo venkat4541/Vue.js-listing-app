c322aee2361ebd57f4e177195e962a3982064cab
Venkat Polumahanti <venkat4541@gmail.com>
2017-03-17 22:56:57 -0400

    Listing of data working.

---
+++ js/script.js
@@ -0,0 +1,33 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    temp: {
+      comments: '',
+      location: '',
+      rooms: '',
+      type: ''
+    },
+    preferences: [
+      {
+        comments: "Prefer to have a balcony",
+        location: "Fairfax",
+        rooms: 3,
+        type: "apartment"
+      },
+      {
+        comments: "Prefer to have a balcony",
+        location: "McLean",
+        rooms: 3,
+        type: "apartment"
+      }
+    ]
+  },
+  methods: {
+    addPref: function() {
+      this.preferences.push(temp);
+    },
+    rem: function(ind) {
+      this.preferences.splice(ind,1)
+    }
+  }
+})
